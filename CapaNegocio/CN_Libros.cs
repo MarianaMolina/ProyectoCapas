cc702b0214c0c7ea388dac4f3e8154fceeba273d
Mariana Molina <37163483+MarianaMolina@users.noreply.github.com>
2021-06-28 23:11:03 -0600

    Add files via upload

---
+++ CapaNegocio/CN_Libros.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CapaDatos;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaNegocio
+{
+    public class CN_Libros
+    {
+        private CD_Libros objetoCD_Libro = new CD_Libros();
+        public DataTable MostrarLibro()
+        {
+            DataTable tabla = new DataTable();
+            tabla = objetoCD_Libro.Mostrar();
+            return tabla;
+        }
+
+        public void InsertarLibro(string ejemplares, string ISBN, string titulo, string autor, string editorial, string anioedicion, string numedicion, string pais, string idioma, string materia, string paginas, string ubicacion, string descripcion)
+        {
+
+            objetoCD_Libro.Insertar( ejemplares,  ISBN,  titulo,  autor,  editorial, Convert.ToInt16(anioedicion),  numedicion,  pais,  idioma,  materia,  paginas,  ubicacion,  descripcion);
+        }
+
+        public void EditarLibro(string ejemplares, string ISBN, string titulo, string autor, string editorial, string anioedicion, string numedicion, string pais, string idioma, string materia, string paginas, string ubicacion, string descripcion, string id)
+        {
+            objetoCD_Libro.Editar(Convert.ToInt16(id),ejemplares, ISBN, titulo, autor, editorial, Convert.ToInt16(anioedicion), numedicion, pais, idioma, materia, paginas, ubicacion, descripcion);
+        }
+
+        public void EliminarLibro(string id)
+        {
+            objetoCD_Libro.Eliminar(Convert.ToInt32(id));
+        }
+    }
+}
---
+++ CapaNegocio/CN_Videos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaNegocio
+{
+    public class CN_Videos
+    {
+        private CD_Videos objetoCD_video = new CD_Videos();
+        public DataTable MostrarVideo()
+        {
+            DataTable tabla = new DataTable();
+            tabla = objetoCD_video.Mostrar();
+            return tabla;
+        }
+
+        public void InsertarVideo(string titulo, string directora, string productora, string Tipo, string anio, string duracion, string pais, string idioma, string subtitulo, string clasificacion, string genero, string sinopsis, string ubicacion)
+        {
+
+            objetoCD_video.Insertar( titulo,  directora,  productora,  Tipo,  anio,  duracion,  pais,  idioma,  subtitulo,  clasificacion,  genero,  sinopsis,  ubicacion);
+        }
+
+        public void EditarVideo(string titulo, string directora, string productora, string Tipo, string anio, string duracion, string pais, string idioma, string subtitulo, string clasificacion, string genero, string sinopsis, string ubicacion, string id)
+        {
+            objetoCD_video.Editar(Convert.ToInt16(id),  titulo,  directora,  productora,  Tipo,  anio,  duracion,  pais,  idioma,  subtitulo,  clasificacion,  genero,  sinopsis,  ubicacion);
+        }
+
+        public void EliminarVideo(string id)
+        {
+            objetoCD_video.Eliminar(Convert.ToInt32(id));
+        }
+    }
+}
